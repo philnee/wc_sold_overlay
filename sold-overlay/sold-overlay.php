24607c849bc53ee2393893bab07998b3fa3a584e
Phil <philneedham85@gmail.com>
2016-08-02 08:02:08 +0100

    started stylesheet

--- sold-overlay/sold-overlay.php
+++ sold-overlay/sold-overlay.php
@@ -11,15 +11,13 @@
         global $product;
 
         if (! $product->is_in_stock()) {
-            echo '<div class="sold-overlay"
-             style="top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    background: rgba(0,0,0,0.6);
-    position: absolute;">Out of stock</div>';
+            echo '<div class="sold-overlay">Out of stock</div>';
         }
     }
 
-    add_action( 'woocommerce_after_shop_loop_item','add_sold_overlay');
+    add_action( 'woocommerce_after_shop_loop_item','add_sold_overlay',4);function myplugin_scripts() {
+    wp_register_style( 'sold-overlay',  plugin_dir_url( __FILE__ ) . 'assets/sold-overlay.css' );
+    wp_enqueue_style( 'sold-overlay' );
+}
+add_action( 'wp_enqueue_scripts', 'myplugin_scripts' );
 ?>
