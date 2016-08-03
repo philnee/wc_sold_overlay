d12ae24436aa8c305a280ff441e17238e8c7dfbe
Phil <philneedham85@gmail.com>
2016-08-03 22:36:33 +0100

    version number

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # wc_sold_overlay
 A sold overlay for woocommerce wordpress plugin  
-<<<<<<< HEAD
-~Current Version:1.0.6~  
-=======
-~Current Version:1.0.6~  
->>>>>>> cfd9c0d33b00b7031500dc26903e9df2e3772020
+
+~Current Version:1.0.7~  
--- sold-overlay/sold-overlay.php
+++ sold-overlay/sold-overlay.php
@@ -4,7 +4,7 @@
     Plugin URI: http://plugins.philneedham.com
     Description: Plugin for displaying overlay for out of stock items on woocommerce
     Author: Phil Needham
-    Version: 1.0.1
+    Version: 1.0.7
     Author URI: http://plugins.philneedham.com
     */
 
